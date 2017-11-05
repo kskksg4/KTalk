b1ea99ca330b2c234ddb3dd3e01baa3ca2caa8ea
Sung-geun <kskksg4@naver.com>
2017-11-05 12:41:11 +0900

    Commit KTalk Androidd

---
+++ app/src/main/java/com/example/doityeah/ktalk/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.doityeah.ktalk;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinearLayout mainactivity_linearlayout;
+    private FirebaseRemoteConfig mFirebaseRemoteConfig;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mainactivity_linearlayout = (LinearLayout)findViewById(R.id.mainactivity_linearlayout);
+
+        mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance();
+        FirebaseRemoteConfigSettings configSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+        mFirebaseRemoteConfig.setConfigSettings(configSettings);
+        mFirebaseRemoteConfig.setDefaults(R.xml.default_config);
+
+        mFirebaseRemoteConfig.fetch(0) // 항상
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            // After config data is successfully fetched, it must be activated before newly fetched
+                            // values are returned.
+                            mFirebaseRemoteConfig.activateFetched();
+                        } else {
+
+                        }
+                        displayMessage();
+                    }
+                });
+    }
+
+    public void displayMessage(){
+        String splash_background = mFirebaseRemoteConfig.getString("splash_background");
+        boolean caps = mFirebaseRemoteConfig.getBoolean("splash_message_caps");
+        String splash_message = mFirebaseRemoteConfig.getString("splash_message");
+
+        mainactivity_linearlayout.setBackgroundColor(Color.parseColor(splash_background));
+
+        if(caps){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(splash_message).setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+
+            builder.create().show();
+        }else{
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/doityeah/ktalk/model/UserModel.java
@@ -0,0 +1,10 @@
+package com.example.doityeah.ktalk.model;
+
+/**
+ * Created by 성근 on 2017-08-18.
+ */
+
+public class UserModel {
+    public String userName;
+    public String profileImageUrl;
+}
